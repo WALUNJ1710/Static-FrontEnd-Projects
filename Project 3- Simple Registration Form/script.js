02da46cbef77e1401e9d45014929995f105a3630
WALUNJ1710 <walunjpratik2001@gmail.com>
2025-03-06 15:53:39 +0530

    Updated Project- html, css and js in differant files

--- Project 3- Simple Registration Form/script.js
+++ Project 3- Simple Registration Form/script.js
@@ -1,17 +1,20 @@
-function checkStrength(){
-    const password = document.getElementById('password').value;
-    const strength = document.getElementById('strength');
-    if (password.length < 4) {
-        strength.style.width = '25%';
-        strength.style.backgroundColor = 'red';
-    } else if (password.length < 8) {
-        strength.style.width = '50%';
-        strength.style.backgroundColor = 'orange';
-    } else if (password.length < 12) {
-        strength.style.width = '75%';
-        strength.style.backgroundColor = 'yellow';
-    } else {
-        strength.style.width = '100%';
-        strength.style.backgroundColor = 'green';
-    }
-}
+const password = document.getElementById('password');
+const strengthBar = document.querySelector('#strength-bar div');
+
+password.addEventListener('input', () => {
+  const val = password.value;
+  let strength = 0;
+
+  if (val.length >= 6) strength += 20;
+  if (/[A-Z]/.test(val)) strength += 20;
+  if (/[0-9]/.test(val)) strength += 20;
+  if (/[^A-Za-z0-9]/.test(val)) strength += 20;
+  if (val.length >= 12) strength += 20;
+
+  strengthBar.style.width = strength + '%';
+
+  if (strength <= 40) strengthBar.style.background = 'red';
+  else if (strength <= 60) strengthBar.style.background = 'orange';
+  else if (strength <= 80) strengthBar.style.background = 'yellow';
+  else strengthBar.style.background = '#00e600';
+});
